4e6fe73ff676bc617cf7344662cbd149b1eaa1f2
Rob Hardt <rhardt@pivotal.io>
2020-06-03 10:35:40 -0700

    modify functionality to scrape a k8s cluster

--- go.mod
+++ go.mod
@@ -7,28 +7,32 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.7.0
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/googleapis/gnostic v0.4.1 // indirect
 	github.com/hashicorp/errwrap v0.0.0-20180715044906-d6c0cd880357 // indirect
 	github.com/hashicorp/go-multierror v0.0.0-20180717150148-3d5d8f294aa0
-	github.com/hashicorp/hcl v0.0.0-20180404174102-ef8a98b0bbce // indirect
+	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.0 // indirect
 	github.com/mattn/go-colorable v0.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
-	github.com/mitchellh/mapstructure v0.0.0-20180715050151-f15292f7a699 // indirect
-	github.com/pelletier/go-toml v0.0.0-20180724185102-c2dbbc24a979 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
-	github.com/spf13/afero v1.1.1 // indirect
-	github.com/spf13/cast v1.2.0 // indirect
-	github.com/spf13/cobra v0.0.0-20180820174524-ff0d02e85550
-	github.com/spf13/jwalterweatherman v0.0.0-20180814060501-14d3d4c51834 // indirect
-	github.com/spf13/pflag v0.0.0-20180821114517-d929dcbb1086 // indirect
-	github.com/spf13/viper v1.1.0
-	github.com/stretchr/testify v1.3.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
+	github.com/stretchr/testify v1.4.0
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v0.0.0-20180816142147-da425ebb7609
-	golang.org/x/sys v0.0.0-20180821044426-4ea2f632f6e9 // indirect
-	golang.org/x/text v0.0.0-20180810153555-6e3c4e7365dd // indirect
-	gopkg.in/yaml.v2 v2.2.1 // indirect
-	sigs.k8s.io/yaml v1.1.0
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	gopkg.in/yaml.v2 v2.2.8
+	k8s.io/api v0.18.3 // indirect
+	k8s.io/apimachinery v0.18.3
+	k8s.io/cli-runtime v0.18.3
+	k8s.io/client-go v0.18.3
+	k8s.io/kubectl v0.18.3
+	k8s.io/utils v0.0.0-20200601170155-a0dff01d8ea5 // indirect
+	sigs.k8s.io/yaml v1.2.0
 )
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"crypto/tls"
-	"errors"
+	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
+	errors2 "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	//"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/dynamic"
+
+	"gopkg.in/yaml.v2"
+	//"k8s.io/client-go/discovery"
+	"k8s.io/client-go/discovery/cached/memory"
+	"k8s.io/client-go/restmapper"
+	//cmdutil "k8s.io/kubectl/pkg/cmd/util"
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
-	"runtime"
 	"strings"
 
 	"github.com/fatih/color"
@@ -21,6 +29,10 @@ import (
 
 	"github.com/instrumenta/kubeval/kubeval"
 	"github.com/instrumenta/kubeval/log"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 var (
@@ -39,8 +51,8 @@ var (
 
 // RootCmd represents the the command to run when kubeval is run
 var RootCmd = &cobra.Command{
-	Short:   "Validate a Kubernetes YAML file against the relevant schema",
-	Long:    `Validate a Kubernetes YAML file against the relevant schema`,
+	Short:   "Validate everything in a K8s cluster against the relevant schema",
+	Long:    `Validate everything in a K8s cluster against the relevant schema`,
 	Version: fmt.Sprintf("Version: %s\nCommit: %s\nDate: %s\n", version, commit, date),
 	Run: func(cmd *cobra.Command, args []string) {
 		if config.IgnoreMissingSchemas && !config.Quiet {
@@ -58,98 +70,127 @@ var RootCmd = &cobra.Command{
 		}
 
 		success := true
-		windowsStdinIssue := false
+		//windowsStdinIssue := false
 		outputManager := kubeval.GetOutputManager(config.OutputFormat)
 
-		stat, err := os.Stdin.Stat()
-		if err != nil {
-			// Stat() will return an error on Windows in both Powershell and
-			// console until go1.9 when nothing is passed on stdin.
-			// See https://github.com/golang/go/issues/14853.
-			if runtime.GOOS != "windows" {
-				log.Error(err)
-				os.Exit(1)
-			} else {
-				windowsStdinIssue = true
-			}
-		}
+		//stat, err := os.Stdin.Stat()
+		//if err != nil {
+		//	// Stat() will return an error on Windows in both Powershell and
+		//	// console until go1.9 when nothing is passed on stdin.
+		//	// See https://github.com/golang/go/issues/14853.
+		//	if runtime.GOOS != "windows" {
+		//		log.Error(err)
+		//		os.Exit(1)
+		//	} else {
+		//		windowsStdinIssue = true
+		//	}
+		//}
 		// Assert that colors will definitely be used if requested
 		if forceColor {
 			color.NoColor = false
 		}
 		// We detect whether we have anything on stdin to process if we have no arguments
 		// or if the argument is a -
-		notty := (stat.Mode() & os.ModeCharDevice) == 0
-		noFileOrDirArgs := (len(args) < 1 || args[0] == "-") && len(directories) < 1
-		if noFileOrDirArgs && !windowsStdinIssue && notty {
-			buffer := new(bytes.Buffer)
-			_, err := io.Copy(buffer, os.Stdin)
-			if err != nil {
-				log.Error(err)
-				os.Exit(1)
-			}
-			schemaCache := kubeval.NewSchemaCache()
-			config.FileName = viper.GetString("filename")
-			results, err := kubeval.ValidateWithCache(buffer.Bytes(), schemaCache, config)
-			if err != nil {
-				log.Error(err)
-				os.Exit(1)
-			}
-			success = !hasErrors(results)
+		//notty := (stat.Mode() & os.ModeCharDevice) == 0
+		//noFileOrDirArgs := (len(args) < 1 || args[0] == "-") && len(directories) < 1
 
-			for _, r := range results {
-				err = outputManager.Put(r)
-				if err != nil {
-					log.Error(err)
-					os.Exit(1)
-				}
-			}
+
+		//build the k8s client from kubeconfig
+		//@todo see if the kubeconfig flag works
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 		} else {
-			if len(args) < 1 && len(directories) < 1 {
-				log.Error(errors.New("You must pass at least one file as an argument, or at least one directory to the directories flag"))
-				os.Exit(1)
-			}
-			schemaCache := kubeval.NewSchemaCache()
-			files, err := aggregateFiles(args)
-			if err != nil {
-				log.Error(err)
-				success = false
-			}
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		//kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
+		//matchVersionKubeConfigFlags := cmdutil.NewMatchVersionFlags(kubeConfigFlags)
+		//f := cmdutil.NewFactory(matchVersionKubeConfigFlags)
+		cfg, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+		clientset, err := kubernetes.NewForConfig(cfg)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
 
-			var aggResults []kubeval.ValidationResult
-			for _, fileName := range files {
-				filePath, _ := filepath.Abs(fileName)
-				fileContents, err := ioutil.ReadFile(filePath)
-				if err != nil {
-					log.Error(fmt.Errorf("Could not open file %v", fileName))
-					earlyExit()
-					success = false
-					continue
-				}
-				config.FileName = fileName
-				results, err := kubeval.ValidateWithCache(fileContents, schemaCache, config)
-				if err != nil {
-					log.Error(err)
-					earlyExit()
-					success = false
-					continue
-				}
+		//grab all the api-group-resources from the cluster
+		apiGroupResources, err := restmapper.GetAPIGroupResources(clientset.DiscoveryClient)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
 
-				for _, r := range results {
-					err := outputManager.Put(r)
+		dyn, err := dynamic.NewForConfig(cfg)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+
+
+		schemaCache := kubeval.NewSchemaCache()
+		var aggResults []kubeval.ValidationResult
+		mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(clientset.DiscoveryClient))
+		//iterate through each group/version/resource
+		for _, apiGroupResource := range apiGroupResources {
+			//log.Success(fmt.Sprintf("API - %v", apiGroupResource))
+			for _, version := range apiGroupResource.Group.Versions {
+				resources := apiGroupResource.VersionedResources[version.Version]
+				//log.Success(fmt.Sprintf("Version - %v", version.Version))
+				for _, resource := range resources {
+					//subresources not of interest
+					if strings.Contains(resource.Name, "/") {
+						continue
+					}
+					//log.Success(fmt.Sprintf("Got resource: %v", resource))
+
+					//grab all the instances of this resource from the cluster
+					gvk := schema.GroupVersionKind{Group: apiGroupResource.Group.Name, Version: version.Version, Kind: resource.Kind}
+					mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
 					if err != nil {
 						log.Error(err)
 						os.Exit(1)
 					}
+
+					dr := dyn.Resource(mapping.Resource)
+					res, err := dr.List(context.Background(), metav1.ListOptions{})
+					if errors2.IsNotFound(err) {
+						//log.Warn(fmt.Sprintf("Nothing found for: %v, %v, %v", mapping.Resource.Group, mapping.Resource.Version, mapping.Resource.Resource))
+						continue
+					} else if _, isStatus := err.(*errors2.StatusError); isStatus {
+						//log.Error(fmt.Errorf("Error getting resource: %v - %v %v %v", statusError, mapping.Resource.Group, mapping.Resource.Version, mapping.Resource.Resource))
+						continue
+					}
+
+					//log.Success(fmt.Sprintf("**** Resource/Instances: %v/%v/%v, %d", mapping.Resource.Group, mapping.Resource.Version, mapping.Resource.Resource, strconv.Itoa(len(res.Items))))
+					for _, i := range res.Items {
+						//log.Success(fmt.Sprintf("  - GOT ME A: %v/%v/%v, %v/%v", i.GroupVersionKind().Group, i.GroupVersionKind().Version, i.GroupVersionKind().Kind, i.GetNamespace(), i.GetName()))
+					 	objYaml, err := yaml.Marshal(i.Object)
+					 	if err != nil{
+							log.Warn(fmt.Sprintf("  - Unable to serialize to YAML: %v/%v/%v, %v/%v", i.GroupVersionKind().Group, i.GroupVersionKind().Version, i.GroupVersionKind().Kind, i.GetNamespace(), i.GetName()))
+						}
+						results, err := kubeval.ValidateWithCache(objYaml, schemaCache, config)
+						r := results[0]
+						err = outputManager.Put(r)
+						if !r.ValidatedAgainstSchema {
+							//log.Warn("...Skippinng additional of same kind")
+							break
+						}
+						if err != nil {
+							log.Error(err)
+							os.Exit(1)
+						}
+
+						aggResults = append(aggResults, results...)
+						success = success && !hasErrors(aggResults)
+					}
 				}
-
-				aggResults = append(aggResults, results...)
 			}
-
-			// only use result of hasErrors check if `success` is currently truthy
-			success = success && !hasErrors(aggResults)
 		}
-
 		// flush any final logs which may be sitting in the buffer
 		err = outputManager.Flush()
 		if err != nil {
@@ -160,6 +201,97 @@ var RootCmd = &cobra.Command{
 		if !success {
 			os.Exit(1)
 		}
+
+
+
+		//if err != nil {
+		//	log.Error(err)
+		//	os.Exit(1)
+		//}
+		//log.Success(fmt.Sprintf("%v", groups))
+		//log.Success(fmt.Sprintf("%v", resources))
+
+
+
+//		if noFileOrDirArgs && !windowsStdinIssue && notty {
+//			buffer := new(bytes.Buffer)
+//			_, err := io.Copy(buffer, os.Stdin)
+//			if err != nil {
+//				log.Error(err)
+//				os.Exit(1)
+//			}
+//			schemaCache := kubeval.NewSchemaCache()
+//			config.FileName = viper.GetString("filename")
+//			results, err := kubeval.ValidateWithCache(buffer.Bytes(), schemaCache, config)
+//			if err != nil {
+//				log.Error(err)
+//				os.Exit(1)
+//			}
+//			success = !hasErrors(results)
+//
+//			for _, r := range results {
+//				err = outputManager.Put(r)
+//				if err != nil {
+//					log.Error(err)
+//					os.Exit(1)
+//				}
+//			}
+//		} else {
+//			if len(args) < 1 && len(directories) < 1 {
+//				log.Error(errors.New("You must pass at least one file as an argument, or at least one directory to the directories flag"))
+//				os.Exit(1)
+//			}
+//			schemaCache := kubeval.NewSchemaCache()
+//			files, err := aggregateFiles(args)
+//			if err != nil {
+//				log.Error(err)
+//				success = false
+//			}
+//
+//			var aggResults []kubeval.ValidationResult
+//			for _, fileName := range files {
+//				filePath, _ := filepath.Abs(fileName)
+//				fileContents, err := ioutil.ReadFile(filePath)
+//				if err != nil {
+//					log.Error(fmt.Errorf("Could not open file %v", fileName))
+//					earlyExit()
+//					success = false
+//					continue
+//				}
+//				config.FileName = fileName
+//				results, err := kubeval.ValidateWithCache(fileContents, schemaCache, config)
+//				if err != nil {
+//					log.Error(err)
+//					earlyExit()
+//					success = false
+//					continue
+//				}
+//
+//				for _, r := range results {
+//					err := outputManager.Put(r)
+//					if err != nil {
+//						log.Error(err)
+//						os.Exit(1)
+//					}
+//				}
+//
+//				aggResults = append(aggResults, results...)
+//			}
+//
+//			// only use result of hasErrors check if `success` is currently truthy
+//			success = success && !hasErrors(aggResults)
+//		}
+//
+//		// flush any final logs which may be sitting in the buffer
+//		err = outputManager.Flush()
+//		if err != nil {
+//			log.Error(err)
+//			os.Exit(1)
+//		}
+//
+//		if !success {
+//			os.Exit(1)
+//		}
 	},
 }
 
